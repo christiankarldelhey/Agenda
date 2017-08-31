bc51a4d5c3f554410a9e4751bbb5a62136862e58
christiankarldelhey <caascaas@gmail.com>
2017-08-31 17:04:52 -0300

    subo

--- Agenda/dist-palta/js/app.js
+++ Agenda/dist-palta/js/app.js
@@ -1,113 +1,49 @@
-
-	$('#menuSingle').mobileSelect();
-	$('#menuMultiple').mobileSelect();
-
-<!-- Autocomplete -->
+//FULLSCREEN SELECT ACTIVATE
+  $('.menuSingle').mobileSelect();
+  $('.menuMultiple').mobileSelect();
 
 
-  $(function() {
-	$('.chosen-select').chosen();
-	$('.chosen-select-deselect').chosen({ allow_single_deselect: true });
+//Datemask dd/mm/yyyy ACTIVATE
+    $("#dateinput").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+    $("[data-mask]").inputmask();
+
+
+//Datepicker ACTIVATE
+  $('input.dateinput').datepicker({
+    language: "es",
+    autoclose: true,
+    clearBtn: true,
+    orientation: "top"
   });
 
 
+//Primera opción gris en selects de bootstrap
 
-    // Tomar el valor del checkbox
-	$('#radioBtn span').on('click', function(){
-        var sel = $(this).data('value');
-        var tog = $(this).data('toggle');
-        $('#'+tog).val(sel);
-        // You can change these lines to change classes (Ex. btn-default to btn-danger)
-        $('span[data-toggle="'+tog+'"]').not('[data-value="'+sel+'"]').removeClass('active btn-primary').addClass('notActive btn-default');
-        $('span[data-toggle="'+tog+'"][data-value="'+sel+'"]').removeClass('notActive btn-default').addClass('active btn-primary');
+    $(document).ready(function(){
+    $(".select-agenda").on("change", function(){
+    $(this).css("color", "#000");
+    });
     });
 
+// CAROUSEL de Fotos
 
-<!-- Clone informacion academica -->
+$('#myCarousel').carousel({
+  interval: false
+})
 
-var regex = /^(.+?)(\d+)$/i;
-var cloneIndex = $(".clonedInput").length;
-function clone(){
-    $(this).parents(".clonedInput").clone()
-        .appendTo("#infoacademica")
-        .attr("id", "clonedInput" +  cloneIndex)
-        .find("*")
-        .each(function() {
-            var id = this.id || "";
-            var match = id.match(regex) || [];
-            if (match.length == 3) {
-                this.id = match[1] + (cloneIndex);
-            }
-        })
-        .on('click', 'button.clone', clone)
-        .on('click', 'button.remove', remove);
-    cloneIndex++;
-}
-function remove(){
-    $(this).parents(".clonedInput").remove();
-}
-$("button.clone").on("click", clone);
-
-$("button.remove").on("click", remove);
-
-
-//iCheck for checkbox and radio inputs
-    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-      checkboxClass: 'icheckbox_minimal-blue',
-      radioClass: 'iradio_minimal-blue'
-    });
-    //Red color scheme for iCheck
-    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-      checkboxClass: 'icheckbox_minimal-red',
-      radioClass: 'iradio_minimal-red'
-    });
-    //Flat red color scheme for iCheck
-    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-      checkboxClass: 'icheckbox_flat-green',
-      radioClass: 'iradio_flat-green'
-    });
-
-  $(function () {
-    //Enable iCheck plugin for checkboxes
-    //iCheck for checkbox and radio inputs
-    $('.mailbox-messages input[type="checkbox"]').iCheck({
-      checkboxClass: 'icheckbox_flat-blue',
-      radioClass: 'iradio_flat-blue'
-    });
-
-    //Enable check and uncheck all functionality
-    $(".checkbox-toggle").click(function () {
-      var clicks = $(this).data('clicks');
-      if (clicks) {
-        //Uncheck all checkboxes
-        $(".mailbox-messages input[type='checkbox']").iCheck("uncheck");
-        $(".fa", this).removeClass("fa-check-square-o").addClass('fa-square-o');
-      } else {
-        //Check all checkboxes
-        $(".mailbox-messages input[type='checkbox']").iCheck("check");
-        $(".fa", this).removeClass("fa-square-o").addClass('fa-check-square-o');
-      }
-      $(this).data("clicks", !clicks);
-    });
-
-    //Handle starring for glyphicon and font awesome
-    $(".mailbox-star").click(function (e) {
-      e.preventDefault();
-      //detect type
-      var $this = $(this).find("a > i");
-      var glyph = $this.hasClass("glyphicon");
-      var fa = $this.hasClass("fa");
-
-      //Switch states
-      if (glyph) {
-        $this.toggleClass("glyphicon-star");
-        $this.toggleClass("glyphicon-star-empty");
-      }
-
-      if (fa) {
-        $this.toggleClass("fa-star");
-        $this.toggleClass("fa-star-o");
-      }
-    });
-  });
-
+$('.carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
